204550153d188c65d7acbf143efe83292fde9b65
Slava Chabanov <chabislav@gmail.com>
2023-11-17 01:57:18 -0800

    first commit

---
+++ trustek-widget-loader.js
@@ -0,0 +1,69 @@
+ (() => {
+
+    const script = document.currentScript;
+
+     window.TrustekWidget = {}
+     TrustekWidget.create = ({elementId}) => {
+         window.addEventListener ?
+             window.addEventListener("load",() => start(elementId),false)
+             :
+             window.attachEvent && window.attachEvent("onload",() => start(elementId));
+    }
+
+    const start = (elementId) => {
+        if ( document.readyState === "complete" ) {
+            console.log('loading widget...');
+            loadWidget(elementId)
+        } else {
+            document.addEventListener("readystatechange", () => {
+                if ( document.readyState === "complete" ) {
+                    console.log('loading widget...');
+                    loadWidget(elementId)
+                }
+            });
+        }
+
+    }
+
+    const loadWidget = (elementId) => {
+        const widgetContainer = document.getElementById(elementId);
+        const widget= document.createElement("div");
+
+        const widgetStyle = widget.style;
+        widgetStyle.display = "none";
+        widgetStyle.boxSizing = "border-box";
+        widgetStyle.overflow = "hidden";
+        widgetStyle.borderRadius = "22px";
+        widgetStyle.width = "300px";
+        widgetStyle.height = "547px";
+        widgetStyle.position = "absolute";
+
+        const iframe = document.createElement("iframe");
+
+        const iframeStyle = iframe.style;
+        // iframeStyle.boxSizing = "borderBox";
+        widgetStyle.borderRadius = "22px";
+        iframeStyle.position = "absolute";
+        iframeStyle.overflow = "hidden";
+        iframeStyle.right = 0;
+        iframeStyle.top = 0;
+        iframeStyle.width = "100%";
+        iframeStyle.height = "100%";
+        iframeStyle.border = 0;
+        iframeStyle.margin = 0;
+        iframeStyle.padding = 0;
+        // iframeStyle.width = "300px";
+
+        widget.appendChild(iframe);
+
+        iframe.addEventListener("load", () => widgetStyle.display = "block" );
+
+        const widgetUrl = `https://trading-widget-pi.vercel.app/dashboard/`;
+
+        iframe.src = widgetUrl;
+        widgetContainer.appendChild(widget);
+
+    }
+
+    return TrustekWidget;
+})();
