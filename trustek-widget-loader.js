f88c3bbe377575c844e64dea290be64d128cee12
Nataliia Syngaievska <nataliia.syngaievska@hoaresbank.co.uk>
2023-12-18 20:06:18 +0200

    Add user id

--- trustek-widget-loader.js
+++ trustek-widget-loader.js
@@ -1,71 +1,64 @@
- (() => {
+;(() => {
+  window.TrustekWidget = {}
+  TrustekWidget.create = ({ elementId, userId }) => {
+    window.addEventListener
+      ? window.addEventListener('load', () => start(elementId, userId), false)
+      : window.attachEvent &&
+        window.attachEvent('onload', () => start(elementId, userId))
+  }
 
-    const script = document.currentScript;
-
-     window.TrustekWidget = {}
-     TrustekWidget.create = ({elementId}) => {
-         loadWidget(elementId)
-         window.addEventListener ?
-             window.addEventListener("load",() => start(elementId),false)
-             :
-             window.attachEvent && window.attachEvent("onload",() => start(elementId));
-    }
-
-    const start = (elementId) => {
-        loadWidget(elementId)
-        if ( document.readyState === "complete" ) {
-            console.log('loading widget...');
-            loadWidget(elementId)
-        } else {
-            document.addEventListener("readystatechange", () => {
-                if ( document.readyState === "complete" ) {
-                    console.log('loading widget...');
-                    loadWidget(elementId)
-                }
-            });
+  const start = (elementId, userId) => {
+    if (document.readyState === 'complete') {
+      console.log('loading widget...')
+      loadWidget(elementId, userId)
+    } else {
+      document.addEventListener('readystatechange', () => {
+        if (document.readyState === 'complete') {
+          console.log('loading widget...')
+          loadWidget(elementId, userId)
         }
-
+      })
     }
+  }
 
-    const loadWidget = (elementId) => {
-        const widgetContainer = document.getElementById(elementId);
-        const widget= document.createElement("div");
+  const loadWidget = (elementId, userId) => {
+    const widgetContainer = document.getElementById(elementId)
+    const widget = document.createElement('div')
 
-        const widgetStyle = widget.style;
-        widgetStyle.display = "none";
-        widgetStyle.boxSizing = "border-box";
-        widgetStyle.overflow = "hidden";
-        widgetStyle.borderRadius = "22px";
-        widgetStyle.width = "300px";
-        widgetStyle.height = "547px";
-        widgetStyle.position = "absolute";
+    const widgetStyle = widget.style
+    widgetStyle.display = 'none'
+    widgetStyle.boxSizing = 'border-box'
+    widgetStyle.overflow = 'hidden'
+    widgetStyle.borderRadius = '22px'
+    widgetStyle.width = '300px'
+    widgetStyle.height = '547px'
+    widgetStyle.position = 'absolute'
 
-        const iframe = document.createElement("iframe");
+    const iframe = document.createElement('iframe')
 
-        const iframeStyle = iframe.style;
-        // iframeStyle.boxSizing = "borderBox";
-        widgetStyle.borderRadius = "22px";
-        iframeStyle.position = "absolute";
-        iframeStyle.overflow = "hidden";
-        iframeStyle.right = 0;
-        iframeStyle.top = 0;
-        iframeStyle.width = "100%";
-        iframeStyle.height = "100%";
-        iframeStyle.border = 0;
-        iframeStyle.margin = 0;
-        iframeStyle.padding = 0;
-        // iframeStyle.width = "300px";
+    const iframeStyle = iframe.style
+    // iframeStyle.boxSizing = "borderBox";
+    widgetStyle.borderRadius = '22px'
+    iframeStyle.position = 'absolute'
+    iframeStyle.overflow = 'hidden'
+    iframeStyle.right = 0
+    iframeStyle.top = 0
+    iframeStyle.width = '100%'
+    iframeStyle.height = '100%'
+    iframeStyle.border = 0
+    iframeStyle.margin = 0
+    iframeStyle.padding = 0
+    // iframeStyle.width = "300px";
 
-        widget.appendChild(iframe);
+    widget.appendChild(iframe)
 
-        iframe.addEventListener("load", () => widgetStyle.display = "block" );
+    iframe.addEventListener('load', () => (widgetStyle.display = 'block'))
 
-        const widgetUrl = `https://trading-widget-pi.vercel.app/dashboard/`;
+    const widgetUrl = `http://localhost:8085/dashboard?userId=${userId}`
 
-        iframe.src = widgetUrl;
-        widgetContainer.appendChild(widget);
+    iframe.src = widgetUrl
+    widgetContainer.appendChild(widget)
+  }
 
-    }
-
-    return TrustekWidget;
-})();
+  return TrustekWidget
+})()
